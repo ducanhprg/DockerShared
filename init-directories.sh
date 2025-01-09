e0fefe59043c628431f37c37fd71f5868412e087
Derek <ducanh.2206@gmail.com>
2025-01-09 16:03:12 +0700

    Add built for Ansible and Terraform, Load balancer

--- init-directories.sh
+++ init-directories.sh
@@ -7,4 +7,8 @@ mkdir -p ./volumes/rabbitmq-data
 mkdir -p ./volumes/grafana-data
 mkdir -p ./volumes/proxysql-data
 mkdir -p ./volumes/tempo-traces
+mkdir -p ./volumes/nginx-config
+mkdir -p ./volumes/terraform-data
+mkdir -p ./volumes/ansible-data
+
 chmod -R 777 ./volumes
